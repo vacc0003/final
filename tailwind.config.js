a11904192d7bee6973e8ed7e8cb944c521cc17e6
vacc0003 <vacc0003@algonquinlive.com>
2024-04-06 21:37:36 -0400

    adds tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix:'tw-',
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      'black': '#000',
+    },
+  },
+  plugins: [],
+}
+
